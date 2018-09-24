7a7c65936e2005abe19cad18a80917d32f1ee165
Jody Hunt <jody.hunt@cyberark.com>
2018-09-24 16:28:18 -0500

    wip

---
+++ config/0_start_minishift.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+set -eou pipefail
+if [[ "$OPENSHIFT_VERSION" == "" ]]; then
+	echo "source config/minishift.config first before running this script."
+	exit -1
+fi
+STARTUP=restart
+if [[ $# == 1 ]]; then
+  STARTUP=$1
+fi
+if [[ $STARTUP != restart ]]; then
+  minishift delete -f
+  rm -rf ~/.kube ~/.minishift
+fi
+minishift start --memory $MINISHIFT_VM_MEMORY --vm-driver virtualbox --show-libmachine-logs --openshift-version $OPENSHIFT_VERSION
+echo ""
+echo "IMPORTANT!  IMPORTANT!  IMPORTANT!  IMPORTANT!"
+echo "You need to source config/global.config again to use Minishift resources."
+echo ""
